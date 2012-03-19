32d0f476b33fad7983dc01301bd2297c086c6ebc
Andrew Heard <andrew.heard@insitedigital.co.uk>
2012-03-19 15:08:53 +0000

    Delete Test FIle

--- gitpull.php
+++ gitpull.php
@@ -1,13 +1,1 @@
-<?php
-// Report all PHP errors
-error_reporting(-1);
-
-// Same as error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-
-$output1 = shell_exec('git pull origin master');
-echo "<pre>$output1</pre>";
-//$output = shell_exec('git help');
-//echo "<pre>$output</pre>";
-//echo "here";
-?>
+<?php `git pull`; ?>
