5aab36e134e530249e491d06319254325f923e36
Andrew Heard <andrew.heard@insitedigital.co.uk>
2012-03-19 14:59:52 +0000

    Delete Test FIle

--- gitpull.php
+++ gitpull.php
@@ -1,7 +1,13 @@
-<?php 'git pull'; 
+<?php
+// Report all PHP errors
+error_reporting(-1);
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
 
+$output1 = shell_exec('git pull origin master');
+echo "<pre>$output1</pre>";
+$output = shell_exec('git help');
+echo "<pre>$output</pre>";
+echo "here";
+?>
