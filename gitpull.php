99edda678c9612e910016bdc16f0f011a57d5208
Andrew Heard <andrew.heard@insitedigital.co.uk>
2012-03-20 09:56:06 +0000

    Delete Test FIle

--- gitpull.php
+++ gitpull.php
@@ -1,9 +1,16 @@
 <?php
-$fh = fopen("logfile.php", "a+" ) 
 
+// Report all PHP errors
+error_reporting(-1);
+
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+$fh = fopen("logfile.php", "a+" ) ;
+$result = "HERE!\n\n";
 ?>
 <?php //`git pull`;
-
+fputs($fh, $result, strlen($result));
 
 define('PROJECTS_PATH', '/var/www/vhosts/gittest.net-development.co.uk/httpdocs');
 /**
@@ -22,15 +29,40 @@ else
 // check for payload and server key
 if ( $payload->ref === 'refs/heads/master' && $_REQUEST['key'] == SERVER_KEY ) {
         // parse the payload for the project name
+	$result = "Into Loop\n\n";
+	fputs($fh, $result, strlen($result));
         $project_name = strtolower($payload->{'repository'}->{'name'});
+		$result = "Payload\n\n";
+	fputs($fh, $result, strlen($result));
         // define the cd directory based on config and project name
         $project_directory = PROJECTS_PATH;
 
+		$result = "Projects\n\n";
+	fputs($fh, $result, strlen($result));
         // cd into the project dir, git reset and pull changes
-        $result = shell_exec( 'cd ' . $project_directory . '/ && git reset --hard HEAD && git pull' );
+		//try{
+			$result = "In try\n\n";
+	fputs($fh, $result, strlen($result));
+        $result = shell_exec('git pull origin master' );
+		$log_result = serialize($result);
+		fputs($fh, $result, strlen($result));
+		$result = "Post Exec\n\n";
+	fputs($fh, $result, strlen($result));
+		//'git pull';
+		//}
+		//catch(Exception $e)
+		{
+		//	fputs($fh, $e, strlen($e));
+		}
+}
+else
+{
+	$result = "Failed Loop\n\n" . $payload->ref . "\n\n" . $_REQUEST['key'] . "\n\n";
+	fputs($fh, $result, strlen($result));
 }
 }
-
+echo "<pre>$result</pre>";
 fputs($fh, $result, strlen($result));
 fclose($fh);
+
 
